4914f00d53000d765c4ef7d99939a87cab59e6db
Ruth Blackman <rkjb410@gmail.com>
2025-05-08 12:38:48 +0100

    add score

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
+@onready var game_manager: Node = %GameManager # Game manager has a unique name 
+# can only be accessed from the same scene
 
-func _on_body_entered(body: Node2D) -> void:
-	print("+1 coin")
+func _on_body_entered(body: Node) -> void:
+	game_manager.add_point()
 	queue_free() # remove scene
