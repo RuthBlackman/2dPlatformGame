6ddf84a1a39c39db5ccc0154138eeb90f760faf2
Ruth Blackman <rkjb410@gmail.com>
2025-05-08 12:57:57 +0100

    add background music and coin pickup sound

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -2,7 +2,8 @@ extends Area2D
 
 @onready var game_manager: Node = %GameManager # Game manager has a unique name 
 # can only be accessed from the same scene
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body: Node) -> void:
 	game_manager.add_point()
-	queue_free() # remove scene
+	animation_player.play("pickup")
